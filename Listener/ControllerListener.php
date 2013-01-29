cd7c5ce9c2cc2a303754c0c6a894306b7ba9396b
Leny Bernard <leny@appventus.com>
2013-01-30 00:58:24 +0100

    preExecute Listener

---
+++ Listener/ControllerListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppVentus\Awesome\ShortcutsBundle\Listener;
+use Symfony\Component\HttpKernel\HttpKernelInterface; 
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use \Symfony\Component\HttpFoundation\Response;
+
+class ControllerListener 
+{	 
+    public function preExecuteController(FilterControllerEvent $event)
+    {
+        // Event catching
+        if(HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()) 
+        {
+            // controller catching    
+            $_controller = $event->getController();
+            if (isset($_controller[0])) 
+            {
+                $controller = $_controller[0];
+                // preExecute method verification
+                if(method_exists($controller,'preExecute'))
+                {
+                    $controller->preExecute();
+                }
+            }
+        }
+ 
+    }
+}
