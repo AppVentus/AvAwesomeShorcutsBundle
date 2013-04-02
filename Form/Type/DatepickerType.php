8eca7429d8a6d53e52e3dfafe006826ee72fb587
shadypierre <shadypierre@gmail.com>
2013-04-02 14:00:24 +0200

    - improve README.md - add datepicker form type - remove unused files

---
+++ Form/Type/DatepickerType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppVentus\Awesome\ShortcutsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DatepickerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $settings = null;
+        if (0 !== count($options['options'])) {
+            $settings = json_encode($options['options']);
+        }
+        $view->vars['datepicker_settings'] = $settings;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'widget'  => 'single_text',
+            'input'   => 'datetime',
+            'format'  => 'dd-MM-yyyy',
+            'options' => array(
+                'dateFormat' => 'dd-mm-yy',
+            ),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'date';
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'datepicker';
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 AvAwesomeShorcutsBundle
 =======================
+
+## Installation
+
+Cette procédure décrit l'installation du projet pour une utilisation dans une machine virtuelle vagrant.
+
+### Récupération du bundle
+#### Composer
+
+Ajouter les lignes suivante dans votre composer.json :
+    {
+        "require": {
+            "pugx/multi-user-bundle": "master-dev"
+        }
+    }
+
+Puis, executer la commande suivante :
+    php composer.phar update
+
+### Activer le bundle
+
+Dans votre AppKernel.php ajouter les lignes suivante :
+
+    <?php
+
+    public function registerBundles()
+    {
+        $bundles = array(
+            // ...
+            new AppVentus\Awesome\ShortcutsBundle\AvAwesomeShorcutsBundle(),
+        );
+    }
