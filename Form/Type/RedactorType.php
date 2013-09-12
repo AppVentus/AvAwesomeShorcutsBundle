0fe2e56ce1034fa96bd004854e3c8eb22083ac39
lenybernard <leny@appventus.com>
2013-09-12 10:13:43 +0200

    Add Redactor library

---
+++ Form/Type/RedactorType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppVentus\Awesome\ShortcutsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RedactorType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $settings = null;
+        if (0 !== count($options['options'])) {
+            $settings = json_encode($options['options']);
+        }
+        $view->vars['redactor_settings'] = $settings;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        // @see http://redactorjs.com/docs/settings
+        $resolver->setDefaults(array(
+            'options' => array(),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'textarea';
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'redactor';
+    }
+
+}
---
+++ Resources/public/libs/redactor/scripts/image_upload.php
@@ -0,0 +1,37 @@
+<?php
+
+// This is a simplified example, which doesn't cover security of uploaded images.
+// This example just demonstrate the logic behind the process.
+
+
+// files storage folder
+// Be carefull, this is depending of your assets strategy (symlink or not)
+$uploadDir = '/uploads/cms/images/';
+$dir = __DIR__.'/../../../../../web'.$uploadDir;
+
+$_FILES['file']['type'] = strtolower($_FILES['file']['type']);
+
+if (
+    $_FILES['file']['type'] == 'image/png'
+    || $_FILES['file']['type'] == 'image/jpg'
+    || $_FILES['file']['type'] == 'image/gif'
+    || $_FILES['file']['type'] == 'image/jpeg'
+    || $_FILES['file']['type'] == 'image/pjpeg')
+{
+    // setting file's mysterious name
+    $filename = md5(date('YmdHis')).'.jpg';
+    $file = $dir.$filename;
+
+    // copying
+    copy($_FILES['file']['tmp_name'], $file);
+
+    // displaying file
+    $array = array(
+    	'filelink' => $uploadDir.$filename
+    );
+
+    echo stripslashes(json_encode($array));
+
+}
+
+?>
