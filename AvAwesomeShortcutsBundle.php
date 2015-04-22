b64740ac5db98b8c7f1734eb41b85db208bdcdc6
Paul Andrieux <paul@appventus.com>
2015-04-22 16:09:43 +0200

    make the monolog logger available anywhere in debug mode via $GLOBALS["logger"]

--- AvAwesomeShortcutsBundle.php
+++ AvAwesomeShortcutsBundle.php
@@ -2,8 +2,17 @@
 
 namespace AppVentus\Awesome\ShortcutsBundle;
 
+use Monolog\Handler\NullHandler;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AvAwesomeShortcutsBundle extends Bundle
 {
+    public function boot()
+    {
+        if ($this->container->getParameter('kernel.debug')) {
+            $GLOBALS['logger'] = $this->container->get('logger');
+        } else {
+            $GLOBALS['logger'] = new NullHandler();
+        }
+    }
 }
